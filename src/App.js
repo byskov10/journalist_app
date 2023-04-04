073a0abdbe810e99304809bee2e5f39828a094e6
byskov10 <viktorbyskov@gmail.com>
2023-04-04 15:43:29 +0200

    sizing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
-function App() {
+function Layout() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button type="button" class="btn btn-secondary">Test Knap</button> 
-        <p>
-          Hello World! :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className='vh-100'>
+      <Row style={{ height: '13%'}}>
+        <Col lg={8} className="bg-primary">
+          <Row>Searchbar</Row>
+        </Col>
+        <Col lg={4} className="bg-secondary">
+          <Row>Dragbar</Row>
+        </Col>
+      </Row>
+      <Row style={{ height: '87%'}}>
+        <Col lg={4}>
+          <Row className='bg-success'style={{ height: '40%'}}>
+            Metrics
+          </Row>
+          <Row className='bg-warning' style={{ height: '60%'}}>
+            Trendline
+          </Row>
+        </Col>
+        <Col lg={8} className='bg-info'>
+          <Row>Bubblechart</Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default App;
+export default Layout;
