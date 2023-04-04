5b5e3731ddb2d5f803fa81985f2903052392d672
byskov10 <viktorbyskov@gmail.com>
2023-04-04 21:57:16 +0200

    searchbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import SearchBar from './Views/Searchbar/Searchbar';
 
 function Layout() {
   return (
     <Container fluid className='vh-100'>
       <Row style={{ height: '13%'}}>
         <Col lg={8} className="bg-primary">
-          <Row>Searchbar</Row>
+          <Row><SearchBar /></Row>
         </Col>
         <Col lg={4} className="bg-secondary">
           <Row>Dragbar</Row>
