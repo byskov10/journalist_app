3c5c10dd64cb81cba1b7ed08cb6322753837b10a
Louis Walker Nash <louis.nash77@gmail.com>
2023-04-11 15:10:36 +0200

    a

--- src/Components/Metric_selector/MetricSlider.js
+++ src/Components/Metric_selector/MetricSlider.js
@@ -1,46 +1,36 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
-import RangeSlider from "react-bootstrap-range-slider";
+import React, { useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-function TimelineSlider() {
-  const [value, setValue] = useState([2018, 2023]);
+const MIN_YEAR = 2015;
+const MAX_YEAR = 2023;
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+function TimelineSlider(props) {
+  const { onYearChange } = props;
+
+  const [yearRange, setYearRange] = useState([MIN_YEAR, MAX_YEAR]);
+
+  const handleYearChange = (newRange) => {
+    setYearRange(newRange);
+    onYearChange(newRange);
   };
 
-  const minLabel = (
-    <div className="text-muted">{`Min: ${value[0]}`}</div>
-  );
-  const maxLabel = (
-    <div className="text-muted">{`Max: ${value[1]}`}</div>
-  );
-  const valueLabel = (
-    <div className="font-weight-bold">{`${value[0]} - ${value[1]}`}</div>
-  );
-
   return (
-    <div className="container-fluid mt-5">
-      <div className="row">
-        <div className="col">
-          <RangeSlider
-            min={2018}
-            max={2023}
-            step={1}
-            value={value}
-            onChange={handleChange}
-            variant="primary"
-            size="lg"
-            tooltip="on"
-            tooltipLabel={(value) => `${value}`}
-            tooltipPlacement="top"
-            tooltipStyle={{ backgroundColor: "#007bff", borderColor: "#007bff" }}
-            allowOverlap
-            range
-            labels={{ min: minLabel, max: maxLabel, value: valueLabel }}
-          />
-        </div>
+    <div>
+      <Slider
+        min={MIN_YEAR}
+        max={MAX_YEAR}
+        range={true}
+        value={yearRange}
+        onChange={handleYearChange}
+        step={1}
+        marks={{
+          [MIN_YEAR]: MIN_YEAR,
+          [MAX_YEAR]: MAX_YEAR,
+        }}
+      />
+      <div>
+        {yearRange[0]} - {yearRange[1]}
       </div>
     </div>
   );
