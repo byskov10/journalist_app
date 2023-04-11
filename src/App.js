94c7feb6f3ec5418f0d9ca3be9aa4c0a2c36017d
Louis Walker Nash <louis.nash77@gmail.com>
2023-04-11 13:02:26 +0200

    nn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import MetricSelector from './Components/Metric_selector/MetricSelector';
+import TimelineSlider from './Components/Metric_selector/MetricSlider';
 
 function GridExample() {
   return (
@@ -12,8 +12,8 @@ function GridExample() {
       </Row>
       <Row style={{ height: "60%" }}>
         <Col xs={4}></Col>
-        <Col xs={4} className="d-flex justify-content-center align-items-center" style={{ backgroundColor: "#f9f9f9", borderRadius: "15px", boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.25)" }}>
-          <MetricSelector />
+        <Col xs={4} style={{ backgroundColor: "#f9f9f9", borderRadius: "15px", boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.25)" }}>
+          <TimelineSlider />
         </Col>
         <Col xs={4}></Col>
       </Row>
--- src/Components/Metric_selector/MetricSlider.js
+++ src/Components/Metric_selector/MetricSlider.js
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
-// MetricSlider component
-function MetricSlider(props) {
-  const { selectedMetric } = props;
-  const [sliderValues, setSliderValues] = useState([selectedMetric.min, selectedMetric.max]);
+// Helper function to format date as YYYY-MM
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  return `${year}-${month}`;
+}
 
-  // Update slider values when selected metric changes
-  useEffect(() => {
-    setSliderValues([selectedMetric.min, selectedMetric.max]);
-  }, [selectedMetric]);
+// TimelineSlider component
+function TimelineSlider() {
+  const [sliderValue, setSliderValue] = useState(new Date('2018-01-01'));
+
+  // Generate marks for each year and month
+  const marks = {};
+  let currentDate = new Date('2018-01-01');
+  const endDate = new Date('2023-12-31');
+  while (currentDate <= endDate) {
+    marks[formatDate(currentDate)] = formatDate(currentDate);
+    currentDate.setMonth(currentDate.getMonth() + 1);
+  }
 
   // Handle slider value changes
-  function handleSliderChange(values) {
-    setSliderValues(values);
+  function handleSliderChange(value) {
+    setSliderValue(new Date(value));
   }
 
   return (
     <div>
       <Slider
-        value={sliderValues}
-        min={selectedMetric.min}
-        max={selectedMetric.max}
+        value={sliderValue}
+        min={new Date('2018-01-01').getTime()}
+        max={new Date('2023-12-31').getTime()}
         onChange={handleSliderChange}
-        range
-        style={{ width: '300px', paddingTop: '30px' }}
+        marks={marks}
+        step={86400000} // one day in milliseconds
+        tipFormatter={(value) => formatDate(new Date(value))}
+        style={{ width: '80%', margin: '0 auto' }}
       />
-      <div>
-        <span style={{ float: 'right' }}>Max: {sliderValues[1]}</span>
-        <span>Min: {sliderValues[0]}</span>
-      </div>
     </div>
   );
 }
 
-export default MetricSlider;
+export default TimelineSlider;
