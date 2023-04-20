a247ad294c297474f6995a2d14a430ceb9f613e6
Louis Walker Nash <louis.nash77@gmail.com>
2023-04-20 09:41:38 +0200

    blue box styled

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -2,53 +2,89 @@ import React, { useState } from 'react';
 import './Searchbar.css'
 import SearchIcon from './SearchIcon';
 
-function SearchBar({data, setSelectedTopic, selectedTopic}) {
+function SearchBar({ data }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [showCross, setShowCross] = useState(false); // new state variable
 
   const handleFilter = (event) => {
     const searchWord = event.target.value;
     const newFilter = data.filter((value) => {
       return value.topic.toLowerCase().includes(searchWord.toLowerCase());
     });
-    
+
+    setSearchInput(searchWord);
     if (searchWord === "") {
-      setFilteredData([])
+      setFilteredData([]);
     } else {
       setFilteredData(newFilter);
     }
-  }
-  const handleSelect = (topic) => {
-    setSelectedTopic(topic); // save selected kategori to state
-    setFilteredData([]);
-    const searchInput = document.querySelector('.searchInputs input');
-    if (searchInput) {
-      searchInput.value = '';
-    }
+    setShowCross(false); // hide cross button when search input changes
   };
+
+  const handleSelect = (topic) => {
+    setSelectedTopic(topic);
+    setSearchInput(topic);
+    setFilteredData([]);
+    setShowCross(true); // show cross button when a topic is selected
+  };
+
+  const handleReset = () => {
+    setSelectedTopic("");
+    setSearchInput("");
+    setShowCross(false); // hide cross button when resetting
+  };
+
   return (
-    <div className='search'>
-      <div className='searchInputs'>
-        <form>
-          <button style={{border: 'none', background: 'none'}}>
-            <SearchIcon />
-          </button>
-          <input type="search" placeholder="Skriv et emne.." onChange={handleFilter}/>
-        </form>
-      </div>
-      {filteredData.length !== 0 && (
-      <div className='dataResult'>
-        {
-        filteredData.slice(0, 15).map((value, key) => {
-          return (
-            <a className='dataItem' onClick={() => handleSelect(value.topic)}>
-              <p>{value.topic}</p>
-            </a>
-          );
-        })}
-      </div>
+    <div className="search">
+      {!selectedTopic ? (
+        <div className="searchInputs">
+          <form>
+            <button style={{border: 'none', backgroundColor: 'transparent'}}>
+              <SearchIcon />
+            </button>
+            <input
+              type="search"
+              placeholder="Skriv et emne.."
+              value={searchInput}
+              onChange={handleFilter}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  setSelectedTopic(searchInput);
+                  setShowCross(true); // show cross button when Enter key is pressed
+                }
+              }}
+            />
+          </form>
+          {filteredData.length !== 0 && (
+            <div className="dataResult">
+              {filteredData.slice(0, 15).map((value, key) => {
+                return (
+                  <a
+                    key={key}
+                    className="dataItem"
+                    onClick={() => handleSelect(value.topic)}
+                  >
+                    <p>{value.topic}</p>
+                  </a>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="searchWord">
+          <span>{selectedTopic}</span>
+          {showCross && (
+            <button className="resetButton" onClick={handleReset}>
+              &#x2715;
+            </button>
+          )}
+        </div>
       )}
     </div>
-  )
+  );
 }
 
 export default SearchBar;
