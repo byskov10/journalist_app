e03edfcd835532d41327ea3c72fd5741c2bc71af
Louis Walker Nash <louis.nash77@gmail.com>
2023-04-20 10:29:29 +0200

    merged

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -5,22 +5,20 @@ import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 
 function UserProfilePicture(props) {
   return (
-<Container fluid className='d-flex flex-column'>
-    <Row className='d-flex align-items-center justify-content-center'>
-      <Col xs={2} style={{border: 'solid black 1px'}}>
+<Container fluid className='d-flex flex-column pe-4'>
+    <Row className='d-flex align-items-center justify-content-end'>
+      <Col xs={2}>
         <Image src={props.userImage} roundedCircle width="60" height="60" />
       </Col>
-      <Col xs={2} style={{border: 'solid black 1px'}}>
-        <p>{props.userName}</p>
-      </Col>
-      <Col xs={2} style={{border: 'solid black 1px'}}>
-      <Button variant="outline-secondary">
-  <>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
-      <path fill="currentColor" d="M7.4 8.4L12 13 16.6 8.4a.5.5 0 0 1 .7.7l-5 5a.5.5 0 0 1-.7 0l-5-5a.5.5 0 0 1 .7-.7z"/>
-    </svg>
-  </>
-</Button>
+      <Col xs={2}>
+        <p style={{margin: '0'}}>{props.userName}</p>
+        <Button variant="outline-secondary" size="sm">
+            <>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+                <path fill="currentColor" d="M7.4 8.4L12 13 16.6 8.4a.5.5 0 0 1 .7.7l-5 5a.5.5 0 0 1-.7 0l-5-5a.5.5 0 0 1 .7-.7z"/>
+              </svg>
+            </>
+        </Button>
 
       </Col>
     </Row>
