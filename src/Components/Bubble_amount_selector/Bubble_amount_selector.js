a0c16ce381b6580b5067fd4707a36e43fbd4edf5
chlo <chlo@itu.dk>
2023-04-20 13:17:16 +0200

    bubllefilter changes

--- src/Components/Bubble_amount_selector/Bubble_amount_selector.js
+++ src/Components/Bubble_amount_selector/Bubble_amount_selector.js
@@ -1,38 +1,20 @@
-import React, { Component } from "react";
-//import "./styles.css";
-import Range from "rc-slider";
-import "rc-slider/assets/index.css";
+import React, { useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-class BubbleSlider extends Component {
-  state = { sliderValues: [50] };
+function BubbleSlider({BubbleAmount, setBubbleAmount}) {
+  //const [value, setValue] = useState(50);
 
-  handleChange = (sliderValues) => {
-    this.setState({ sliderValues });
+  const handleSliderChange = (newValue) => {
+    setBubbleAmount(newValue);
   };
 
-  render() {
-    const { sliderValues } = this.state;
-
-    return (
-      <div className="mka__range-alignment">
-        <div className="mka__range-text">
-          <p>Antal Bobler: {sliderValues[0]}</p>
-        </div>
-        <div className="mka__range-btn-align">
-          <div className="mka__range-width">
-            <Range
-              range
-              onChange={this.handleChange}
-              defaultValue={[50]}
-              min={0}
-              max={100}
-              //min og maks skal komme an på hvor mange bobler der er
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+            <p>Value: {BubbleAmount}</p>
+      <Slider min={0} max={100} value={BubbleAmount} onChange={handleSliderChange} />
+    </div>
+  );
 }
 
 export default BubbleSlider;
